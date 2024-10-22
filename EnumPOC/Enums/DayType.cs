e9a6b5c80fa170b495ab27b15bb39c3a5fdd9879
Harshvardhan Gupta <76556011+harshvardhan2002@users.noreply.github.com>
2024-10-22 19:14:18 +0530

    Add files via upload

---
+++ EnumPOC/Enums/DayType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPOC.Enums
+{
+    public enum DayType
+    {
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY
+    }
+}
---
+++ EnumPOC/Program.cs
@@ -0,0 +1,24 @@
+﻿using EnumPOC.Enums;
+
+namespace EnumPOC
+{
+    internal class Program
+    {
+        const int MINRANGE = 0;
+        const int MAXRANGE = 6;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the day it was yesterday (in form of 0-6 according to ascending order): ");
+            int day=Convert.ToInt32(Console.ReadLine());
+
+            if (day >= MINRANGE && day <= MAXRANGE)
+            {
+                int todayIndex = (day +1) % (MAXRANGE+1);
+                DayType today = (DayType)todayIndex;
+                Console.WriteLine("Today is: " + today);
+                return;
+            }
+            Console.WriteLine("Enter valid number");
+        }
+    }
+}
